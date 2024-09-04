cec8c2ddbcc9a0c2d5e3e4fea6900b4d6da64c2f
Lonneke van Brussel <125465018+lonnekevanbrussel@users.noreply.github.com>
2024-09-04 15:38:45 +0200

    Update fpkmratio_addexoncounts.R
    Added extra comments

--- scripts/fpkmratio_addexoncounts.R
+++ scripts/fpkmratio_addexoncounts.R
@@ -1,3 +1,6 @@
+# Code to replace OUTRIDER exon-ratio results with corresponding exon-level results for raw counts - normalized counts and meanCorrected
+# OUTRIDER exon ratio results are filtered (STEP 1 - 4) based on the exon level counts results
+
 suppressPackageStartupMessages(library("argparse"))
 suppressPackageStartupMessages(library("readr"))
 suppressPackageStartupMessages(library("dplyr"))
@@ -11,15 +14,18 @@ parser$add_argument("-o","--output_path", metavar = "output_path", nargs = "+",
                     help = "Path where output file with replaced normalized and mean correct values.", default="./")
 args <- parser$parse_args()
 
-#url <- "/Users/lbrussel/rnaseq-emc/notebook/rnaseq-voila-main/downloads/protein_coding/finalrefset_ratiofpkm/umcu_rnaseq_fib_untreated_res_outrider_exons_counts.tsv"
+# Get the exon ratio results file
 umc_res_fpkm <- read_delim(args$ratioresult,show_col_types = FALSE)
 
-#url <- "/Users/lbrussel/rnaseq-emc/notebook/rnaseq-voila-main/downloads/protein_coding/finalrefset_cntrl/umcu_rnaseq_res_outrider_exon_counts_L00022_pheno_added.tsv"
+# Get the exon level results file
 umc_res_e <- read_delim(args$exonresult,show_col_types = FALSE)
 
+# Get the correct columns and merge the files by geneID and sampleID
 umc_res_e <- umc_res_e[,c(1,2,7:9)]
 umc_res_merge <- right_join(umc_res_e, umc_res_fpkm, by=c("geneID","sampleID"))
+# SELECT the correct columns to create exon ratio output results with correct exon-level counts
 res_merge_out <- umc_res_merge[,c(1:9,13:25)]
 colnames(res_merge_out)[3:5] <- c("rawcounts","normcounts","meanCorrected")
-#write_csv(res_merge_out, "/Users/lbrussel/rnaseq-emc/notebook/rnaseq-voila-main/downloads/protein_coding/finalrefset_ratiofpkm/umcu_rnaseq_fib_untreated_res_outrider_exons_counts_countsadded.tsv", append=FALSE, col_names = TRUE)
+
+# write the ratio results file with corresponding exon-level raw counts - normalized counts and meanCorrected results
 write_csv(res_merge_out, paste0(args$output_path,"umcu_rnaseq_fib_untreated_res_outrider_exons_counts_countsadded.tsv"), append=FALSE, col_names = TRUE)
